9186ae835235061a113b72a1ba5b783561ac08fd
Cleave-IWNL <stacker99@proton.me>
2025-07-23 21:06:03 +0300

    Initialize token

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+)
 
 func main() {
+	t := mustToken()
+
+	if err != nil {
+		//handle error
+	}
+	// token = flags.Get(token)
+
+	// tgClient = telegram.New(token)
+
+	// fetcher = fetcher.New()
+
+	// processor = processor.New()
+
+	// consumer = consumer.New()
+
 	fmt.Println("Hello, World!")
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specifiedS")
+	}
+
+	return *token
 }
